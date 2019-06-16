24dd6d6a5b34fdbb2686797eec4cb99322c36d90
Manasa Patil <sanama87@gmail.com>
2019-06-16 18:57:23 +0530

    Get Past Orders of User - “/order”

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/OrderController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
 import com.upgrad.FoodOrderingApp.api.model.*;
+import com.upgrad.FoodOrderingApp.service.businness.CustomerService;
 import com.upgrad.FoodOrderingApp.service.businness.OrderService;
 import com.upgrad.FoodOrderingApp.service.entity.CouponEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.entity.OrderEntity;
 import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
 import com.upgrad.FoodOrderingApp.service.exception.CouponNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,8 +25,12 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private CustomerService customerService;
+
     /**
      * @param  authorization the first {@code String} to check if the access is available.
+     * @param couponName Coupon name to be looked for
      * @return ResponseEntity is returned with Status OK.
      */
     @RequestMapping(method = RequestMethod.GET, path = "/order/coupon/{coupon_name}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -36,32 +44,69 @@ public class OrderController {
             bearerToken = authorization;
         }
 
-        //Check if the coupon name is empty
-        if(couponName.isEmpty())
-        {
-            throw new CouponNotFoundException("CPF-002",
-                    "Coupon name field should not be empty");
-        }
+        //Validate customer
+        CustomerEntity customerEntity = customerService.getCustomer(bearerToken);
 
         // Get coupon details
-        CouponEntity couponDetails = orderService.getCoupon(couponName,bearerToken);
+        CouponEntity couponDetails = orderService.getCoupon(couponName);
 
         // Create response
-        CouponDetailsResponse couponDetailsResponse = new CouponDetailsResponse();
-        if(couponDetails==null)
-        {
-            couponDetailsResponse = null;
-            throw new CouponNotFoundException("CPF-001",
-                    "No coupon by this name");
-        } else {
-
-                     couponDetailsResponse.id(UUID.fromString(couponDetails.getUuid()))
-                    .couponName(couponDetails.getCouponName())
-                    .percent(couponDetails.getPercent());
-        }
+        CouponDetailsResponse couponDetailsResponse = new CouponDetailsResponse()
+                .id(UUID.fromString(couponDetails.getUuid()))
+                .couponName(couponDetails.getCouponName())
+                .percent(couponDetails.getPercent());;
 
         // Return coupon details response
         return new ResponseEntity<CouponDetailsResponse>(couponDetailsResponse, HttpStatus.OK);
     }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/order", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<OrderList>> getPastOrdersByCustomer(@RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, CouponNotFoundException {
+        // Logic to handle Bearer <accesstoken>
+        // User can give only Access token or Bearer <accesstoken> as input.
+        String bearerToken = null;
+        try {
+            bearerToken = authorization.split("Bearer ")[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            bearerToken = authorization;
+        }
+
+        //Validate customer
+        CustomerEntity customerEntity = customerService.getCustomer(bearerToken);
+
+        // Get all past orders
+        List<OrderEntity> allPastOrders = orderService.getAllPastOrdersByCustomer(customerEntity);
+
+        // Create response
+        List<OrderList> allPastOrdersResponses = new ArrayList<OrderList>();
+
+        /*for (int i = 0; i < allPastOrders.size(); i++) {
+            OrderList orderDetailsResponse = new OrderList()
+                    .id(allPastOrders.);
+            allPastOrdersResponses.add(orderDetailsResponse);
+        }*/
+
+        // Return response
+        return new ResponseEntity<List<OrderList>>(allPastOrdersResponses, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/order", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SaveOrderResponse> saveOrderByCustomer(final SaveOrderRequest saveOrderRequest,@RequestHeader("authorization") final String authorization) throws AuthorizationFailedException, CouponNotFoundException {
+        // Logic to handle Bearer <accesstoken>
+        // User can give only Access token or Bearer <accesstoken> as input.
+        String bearerToken = null;
+        try {
+            bearerToken = authorization.split("Bearer ")[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            bearerToken = authorization;
+        }
+        // Creating entity for further update
+
+
+        // Return response
+
+        SaveOrderResponse saveOrderResponse = new SaveOrderResponse();
+        return new ResponseEntity<SaveOrderResponse>(saveOrderResponse, HttpStatus.OK);
+
+    }
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/OrderService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/OrderService.java
@@ -3,16 +3,22 @@ package com.upgrad.FoodOrderingApp.service.businness;
 import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
 import com.upgrad.FoodOrderingApp.service.dao.OrderDao;
 import com.upgrad.FoodOrderingApp.service.entity.CouponEntity;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.entity.OrderEntity;
 import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
+import com.upgrad.FoodOrderingApp.service.exception.CouponNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderDao orderDao;
-
 
     @Autowired
     private CustomerDao customerDao;
@@ -23,20 +29,31 @@ public class OrderService {
 
     /**
      * @param  coupon_name Name of the coupon to be accessed
-     * @param  accessToken the first {@code String} to check if the access is available.
      * @return CouponEntity object.
      */
-    public CouponEntity getCoupon(String coupon_name, String accessToken) throws AuthorizationFailedException {
+    public CouponEntity getCoupon(String coupon_name) throws  CouponNotFoundException {
 
         CouponEntity couponEntity = null;
-        //boolean validateCustomer = customerService.checkCustomer(accessToken);
-        // @Manasa
-        // We should vaidate in Contoroller itself.
-        // Call the customerService.getCustomer from contorller & throw auth exception
-        //if(validateCustomer){
-            couponEntity = orderDao.getCouponByName(coupon_name);
-        //}
+
+        //Check if the coupon name is empty
+        if(coupon_name.isEmpty())
+        {
+            throw new CouponNotFoundException("CPF-002",
+                    "Coupon name field should not be empty");
+        }
+
+        couponEntity = orderDao.getCouponByName(coupon_name);
+
+        if(couponEntity==null)
+        {
+            throw new CouponNotFoundException("CPF-001",
+                    "No coupon by this name");
+        }
         return couponEntity;
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
+    public List<OrderEntity> getAllPastOrdersByCustomer(CustomerEntity customerEntity){
+        return orderDao.getOrdersByCustomers(customerEntity);
+    }
 }
