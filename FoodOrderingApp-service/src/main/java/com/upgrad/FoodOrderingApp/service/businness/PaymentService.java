e72d4da0a2eb767b46e61c1682f8e84298133355
ishita169 <ishuarials@gmail.com>
2019-06-16 17:08:45 +0530

    Add payment in Rest API Endpoints-III

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/PaymentService.java
@@ -1,4 +1,33 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.upgrad.FoodOrderingApp.service.dao.PaymentDao;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+
+@Service
 public class PaymentService {
+	@Autowired
+	private PaymentDao paymentDao;
+	
+	/**
+	 * This method implements the business logic for 'payment' endpoint
+	 *
+	 * @return List<PaymentEntity> object
+	 */
+	public List<PaymentEntity> getAllPaymentMethodByName() {
+		
+		return paymentDao.getAllPaymentMethod().stream()
+				.sorted(Comparator.comparing(PaymentEntity::getPaymentName))
+				.collect(Collectors.toList());
+		
+		
+	}
+
 }
+
+
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/PaymentDao.java
@@ -1,4 +1,31 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import com.upgrad.FoodOrderingApp.service.entity.PaymentEntity;
+
+@Repository
 public class PaymentDao {
+	
+	@PersistenceContext
+	private EntityManager entitymanager;
+	
+	/**
+	 * This method finds all PaymentMethod
+	 *
+	 * @return List<PaymentEntity> object
+	 */
+	public List<PaymentEntity> getAllPaymentMethod() {
+		try {
+		return entitymanager.createNamedQuery("allPaymentMethods", PaymentEntity.class).getResultList();	
+			
+		} catch (NoResultException nre) {
+			return null;
+		}
+	}
+
 }
