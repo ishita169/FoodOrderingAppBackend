de21a1a42724bcb4cada4aab7304ae964fd7ec69
ishita169 <ishuarials@gmail.com>
2019-06-16 05:30:17 +0530

    Category List changes

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
@@ -1,4 +1,38 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.upgrad.FoodOrderingApp.api.model.CategoryListResponse;
+import com.upgrad.FoodOrderingApp.service.businness.CategoryService;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+
+@RestController 
 public class CategoryController {
+	@Autowired
+	private CategoryService categoryservice;
+	
+		
+	@RequestMapping("/category")
+    public List<CategoryListResponse> getAllCategory() {
+		List<CategoryListResponse> categoryList = new ArrayList<>();
+		List<CategoryEntity> categories =categoryservice.getAllCategory();
+		
+		if(categories!=null && categories.size()>0) {
+			for(CategoryEntity categoryEntity : categories) {
+				CategoryListResponse category = new CategoryListResponse();
+				category.setCategoryName(categoryEntity.getCategoryName());
+				category.setId(UUID.fromString(categoryEntity.getUuid()));
+				categoryList.add(category);
+			}
+		}
+       return categoryList;
+    }
+
 }
+
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -1,4 +1,23 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+
+@Service
 public class CategoryService {
+	@Autowired
+	private CategoryDao categoryDao;
+	
+	public List<CategoryEntity> getAllCategory(){
+		return categoryDao.getAllCategory();
+	}
+	
+	public CategoryEntity getCategory(String categoryId){
+		return categoryDao.getCategory(categoryId);
+	}
 }
